ed45adcd162b338f1fa651399b9726ff86a68ddc
Missael Padilla <lmpadillar@gmail.com>
2022-08-11 23:40:23 -0700

    First Commint to mater

--- src/app/pages/notpagefound/notpagefound.component.ts
+++ src/app/pages/notpagefound/notpagefound.component.ts
@@ -3,11 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-notpagefound',
   templateUrl: './notpagefound.component.html',
-  styles: [
-  ]
+  styleUrls: ['./notpagefound.page.css']
 })
 export class NotpagefoundComponent implements OnInit {
 
+  year = new Date().getFullYear();
   constructor() { }
 
   ngOnInit(): void {
