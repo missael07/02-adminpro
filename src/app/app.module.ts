6a2cfa1b8e25d91139727636c09a2022ce27a891
Missael Padilla <lmpadillar@gmail.com>
2022-08-11 22:16:19 -0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NotpagefoundComponent } from './pages/notpagefound/notpagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SigninComponent,
+    RegisterComponent,
+    NotpagefoundComponent,
+    DashboardComponent,
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
